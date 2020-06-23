cb09c48a24312ada7f45a8f355635766c7240080
Max <example@gmail.com>
2020-06-23 21:12:14 +0300

    first commit

---
+++ js/script1.js
@@ -0,0 +1,5 @@
+// const answers = [];
+// answers[0] = prompt('Whats ur name', '');
+// answers[1] = prompt('Whats ur surnmae', '');
+// answers[2] = prompt('How old are u', '');
+// document.write(answers);
