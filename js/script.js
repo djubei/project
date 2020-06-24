39dd5812e26701e3375563b62c6581ff04f46a0e
Max <example@gmail.com>
2020-06-24 19:35:35 +0300

    film rating app

--- js/script.js
+++ js/script.js
@@ -124,8 +124,38 @@ console.log(isChecked || isClose); //* означает "or"
 
 
 
+//!первая дз
+
+'use strict';
 
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', `?`);
+
+
+
+
+const a = prompt(`Один из последних просмотренных фильмов?`, `?`),
+	b = +prompt(`На сколько оцените его?`, `?`),
+	c = prompt(`Один из последних просмотренных фильмов?`, `?`),
+	d = +prompt(`На сколько оцените его?`, `?`);
+
+
+
+
+
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+
+};
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
 
 
--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,31 @@
-// const answers = [];
-// answers[0] = prompt('Whats ur name', '');
-// answers[1] = prompt('Whats ur surnmae', '');
-// answers[2] = prompt('How old are u', '');
-// document.write(answers);
+'use strict';
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', `?`);
+
+
+
+
+const a = prompt(`Один из последних просмотренных фильмов?`, `?`),
+	b = +prompt(`На сколько оцените его?`, `?`),
+	c = prompt(`Один из последних просмотренных фильмов?`, `?`),
+	d = +prompt(`На сколько оцените его?`, `?`);
+
+
+
+
+
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+
+};
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
